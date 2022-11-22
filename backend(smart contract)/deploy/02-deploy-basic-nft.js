53c84d4d1acd1cdf438373baad5dac5bee1069c5
kmarv <marvkaay@gmail.com>
2022-11-22 12:59:43 +0300

    second commit

--- backend(smart contract)/deploy/02-deploy-basic-nft.js
+++ backend(smart contract)/deploy/02-deploy-basic-nft.js
@@ -1,29 +1,27 @@
 const { network } = require("hardhat")
-const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+
+module.exports= async({getNamedAccounts, deployments}) =>{
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const waitBlockConfirmations = developmentChains.includes(network.name)
         ? 1
         : VERIFICATION_BLOCK_CONFIRMATIONS
 
-    log("----------------------------------------------------")
-    const arguments = []
-    const nftMarketplace = await deploy("NFTMarketplace", {
-        from: deployer,
-        args: arguments,
-        log: true,
+    const args = []
+    const basicNFT = await deploy("BasicNft", {
+        from:deployer,
+        args:args,
+        log:true,
         waitConfirmations: waitBlockConfirmations,
     })
-
-    // Verify the deployment
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
-        await verify(nftMarketplace.address, arguments)
+        await verify(basicNFT.address, args)
     }
     log("----------------------------------------------------")
 }
 
-module.exports.tags = ["all", "nftmarketplace"]
+module.exports.tags = ["all", "basicnft"]
